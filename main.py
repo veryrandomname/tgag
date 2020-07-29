bc9ce05537ad675887cbcfdebcba07e976f259d4
seriousemailadress@googlemail.com <seriousemailadress@googlemail.com>
2020-07-29 19:09:08 +0200

    about löschen

--- main.py
+++ main.py
@@ -218,10 +218,5 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/about', methods=['GET'])
-def about():
-    return render_template("about.html")
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
