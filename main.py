3698541a57071d70cc2b295daf8bd9a034546895
Ubuntu <ubuntu@ip-172-26-12-8.eu-central-1.compute.internal>
2020-08-22 13:56:06 +0000

    it finally works

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def get_thumbnail_url(filename):
     if __name__ == "__main__":
         return url_for('thumbnails', filename=filename, _external=True)
     else:
-        return f"https://tgag.app/thumbnails/{filename}.webp"
+        return f"https://tgag.app/thumbnails/{filename}"
 
 
 @app.route('/thumbnails/<filename>', methods=['GET'])
