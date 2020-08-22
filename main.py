402ae97e964537cef4268c57030a38ddf80da318
seriousemailadress@googlemail.com <seriousemailadress@googlemail.com>
2020-08-22 15:31:16 +0200

    - show thumbnails of videos in myuploads - new app version

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import bcrypt
-from flask import (Flask, render_template, g)
+from flask import (Flask, render_template, g, send_from_directory)
 from flask import jsonify
 from flask import request, session, redirect, url_for, escape
 from flask_uploads import (UploadSet, configure_uploads, patch_request_class, UploadConfiguration, IMAGES)
@@ -10,9 +10,8 @@ import dbclient
 app = Flask(__name__)
 app.secret_key = b'as90dhjaSJAaAsafgAF6a6aa36as4DA1'
 
+patch_request_class(app, 1024 * 1024 * 10)  # 10MB file size max
 
-
-patch_request_class(app,1024 * 1024 * 10) #10MB file size max
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -48,17 +47,22 @@ def close_db(exception):
     if hasattr(g, 'db'):
         g.db.tear_down_connection()
 
+
 ALLOWED_EXTENSIONS = tuple('jpg jpe jpeg png webp webm mp4'.split())
-photos = UploadSet('photos', default_dest=lambda app: app.root_path + "/uploads", extensions = ALLOWED_EXTENSIONS)
+photos = UploadSet('photos', default_dest=lambda app: app.root_path + "/uploads", extensions=ALLOWED_EXTENSIONS)
 if __name__ != "__main__":
-    photos._config = UploadConfiguration(app.root_path + "/uploads", base_url="https://tgag.app/_uploads/photos/" )
+    photos._config = UploadConfiguration(app.root_path + "/uploads", base_url="https://tgag.app/_uploads/photos/")
 configure_uploads(app, photos)
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if logged_in() and request.method == 'POST' and 'photo' in request.files:
         filename = photos.save(request.files['photo'])
 
+        name, extension = os.path.splitext(filename)
+        if extension == "webm" or extension == "mp4":
+            create_thumbnail(filename)
         """
         img = Image.open(app.root_path + "/uploads/"+filename)
         img.thumbnail((900,1750))
@@ -77,13 +81,36 @@ def upload():
         return "you not logged in brotha"
 
 
+def create_thumbnail(filename):
+    if not os.path.isfile(f"{app.root_path}/thumbnails/{filename}.webp"):
+        os.system(f"ffmpeg -i {app.root_path}/uploads/{filename} -ss 00:00:00.000 -vframes 1 {app.root_path}/thumbnails/{filename}.webp")
+
+
+def get_thumbnail_url(filename):
+    create_thumbnail(filename)
+
+    return url_for('thumbnails', filename=filename, _external=True)
+
+
+@app.route('/thumbnails/<filename>', methods=['GET'])
+def thumbnails(filename):
+    return send_from_directory(f'{app.root_path}/thumbnails', filename + ".webp", as_attachment = False, mimetype='image')
+
+
+def get_better_upload_overview(userID):
+    users_uploads = get_db().get_upload_overview(current_user())
+    uploads_with_info = []
+    for (itemID, filename, rating) in users_uploads:
+        info = get_item_info(itemID)
+        info["rating"] = rating
+        uploads_with_info += [info]
+    return uploads_with_info
+
+
 @app.route('/your_memes', methods=['GET'])
 def your_memes():
     if logged_in():
-        users_uploads = get_db().get_upload_overview(current_user())
-
-        uploads_with_urls = [(itemID, photos.url(filename), rating) for (itemID, filename, rating) in users_uploads]
-        return render_template('show_uploads.html', uploads=uploads_with_urls)
+        return render_template('show_uploads.html', uploads=get_better_upload_overview(current_user()))
     else:
         return "you not logged in brotha"
 
@@ -91,12 +118,13 @@ def your_memes():
 @app.route('/my_uploads_app', methods=['GET'])
 def my_uploads_app():
     if logged_in():
-        users_uploads = get_db().get_upload_overview(current_user())
+        # users_uploads = get_db().get_upload_overview(current_user())
 
-        uploads_with_urls = [{ "itemID": itemID, "url": photos.url(filename), "rating" : rating } for (itemID, filename, rating) in users_uploads]
-        return jsonify({ "my_uploads" : uploads_with_urls})
+        # uploads_with_urls = [{ "itemID": itemID, "url": photos.url(filename), "rating" : rating } for (itemID, filename, rating) in users_uploads]
+        return jsonify({"my_uploads": get_better_upload_overview(current_user())})
     else:
-        return "you not logged in brotha",500
+        return "you not logged in brotha", 500
+
 
 @app.route('/top_json')
 def top_json():
@@ -111,6 +139,7 @@ def file_extension(filename):
     _, file_extension = os.path.splitext(filename)
     return file_extension[1:]
 
+
 def meme_type(filename):
     ext = file_extension(filename)
     if ext == "mp4" or ext == "webm":
@@ -119,19 +148,25 @@ def meme_type(filename):
         return "image"
 
 
+def get_item_info(itemID):
+    pic = get_db().get_pic(itemID)
+    filename = pic["filename"]
+    t = meme_type(filename)
+    ext = file_extension(filename)
+    thumb = get_thumbnail_url(filename)
+    return {"itemID": itemID, "url": photos.url(pic["filename"]), "author": pic["username"], "file_extension": ext,
+            "type": t, "filename": filename, "thumbnail_url" : thumb}
+
+
 @app.route('/top_urls_json')
 def top_urls_json():
     if logged_in():
         top = get_db().get_top_n(current_user(), 20)
-        urls = []
-        for itemID in top:
-            pic = get_db().get_pic(itemID)
-            t = meme_type(pic["filename"])
-            ext = file_extension(pic["filename"])
-            urls.append({ "itemID" : itemID, "url" : photos.url(pic["filename"]), "author" : pic["username"], "file_extension" : ext, "type" : t, "filename" : pic["filename"]})
+        urls = [get_item_info(itemID) for itemID in top]
         return jsonify({"top_rec": urls})
     else:
         return "Error", 500
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -141,7 +176,7 @@ def home():
             itemID = top[0]
             filename = get_db().get_pic(itemID)["filename"]
             url = photos.url(filename)
-            return render_template('home.html', memeID=itemID, memeurl=url, m_type = meme_type(filename))
+            return render_template('home.html', memeID=itemID, memeurl=url, m_type=meme_type(filename))
         else:
             return render_template('home.html')
     else:
@@ -255,7 +290,6 @@ def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
     return redirect(url_for('home'))
-
 
 
 if __name__ == "__main__":
